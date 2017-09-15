849efaef20557df8ae5e463363c17ad0b63ec220
jghoover <30665181+jghoover@users.noreply.github.com>
2017-09-15 10:39:05 -0400

    Added javadoc comments

--- Stack.java
+++ Stack.java
@@ -1,11 +1,44 @@
 
-public interface Stack
+public interface Stack 
 {
+	/**
+	 * Add an object to the stack
+	 * @param x object to be added to the stack
+	 */
 	public void push(Object x);
+	
+	/**
+	 * Remove and return the top object of the stack
+	 * @return object removed from the stack
+	 */
 	public Object pop();
+	
+	/**
+	 * Return the top object of the stack, but don't remove it
+	 * @return object on top of the stack
+	 */
 	public Object peek();
+	
+	/**
+	 * Check if there are no objects in the stack
+	 * @return true if there are no objects in the stack, false otherwise
+	 */
 	public boolean isEmpty();
+	
+	/**
+	 * Check if no more objects can be added to the stack
+	 * @return true if no more objects can be added to the stack, false otherwise
+	 */
 	public boolean isFull();
+	
+	/**
+	 * Number of objects in the stack
+	 * @return number of objects in the stack
+	 */
 	public int size();
+	
+	/**
+	 * Revert the stack to an empty state
+	 */
 	public void makeEmpty();
 }
--- StackArray.java
+++ StackArray.java
@@ -1,16 +1,28 @@
-//implementation of a stack, using an array
-//note: an array has a defined size, so the stack will have a max size
+/**
+ * Implementation of a stack using an array.
+ * Note that as an array has a predefined size, so this stack implementation will have a predefined size.
+ * @author Mr. Hoover and the class
+ *
+ */
 public class StackArray implements Stack
 {
 	private Object [] data;
 	private int top;
 	
+	/**
+	 * Default constructor.
+	 * @param maxSize maximum number of elements that can be in the stack
+	 */
 	public StackArray(int maxSize)
 	{
 		data = new Object[maxSize];
 		top = -1;
 	}
 	
+	/**
+	 * If the stack is not full, add the element.  If it is full, print an error message.
+	 * @param x object to be pushed to the stack
+	 */
 	public void push(Object x)
 	{
 		if(!isFull())
@@ -23,36 +35,63 @@ public class StackArray implements Stack
 		}
 	}
 	
+	/**
+	 * Remove an object from the stack and return that object.
+	 * @return the object on the top of the stack, or null iff it is empty.
+	 */
 	public Object pop()
 	{
 		return (isEmpty())? null : data[top--];
 	}
 	
+	/**
+	 * Return the object on top of the stack, but do not remove it
+	 * @return the object on top of the stack, or null iff it is empty.
+	 */
 	public Object peek()
 	{
 		return (isEmpty())? null : data[top];
 	}
 	
+	/**
+	 * Check if the stack is empty
+	 * @return false if there are objects in the stack, or true otherwise.
+	 */
 	public boolean isEmpty()
 	{
 		return top<0;
 	}
 	
+	/**
+	 * Check if the stack is full
+	 * @return true if no more objects can be pushed to the stack, or false otherwise
+	 */
 	public boolean isFull()
 	{
 		return top >= data.length - 1;
 	}
 	
+	/**
+	 * Number of objects in the stack
+	 * @return number of objects in the stack
+	 */
 	public int size()
 	{
 		return top + 1;
 	}
 	
+	/**
+	 * Revert the stack to an empty state
+	 */
 	public void makeEmpty()
 	{
 		top = -1;
 	}
 	
+	/**
+	 * Maximum number of elements that can be in the stack at one time
+	 * @return maximum number of objects that can be in the stack
+	 */
 	public int maxSize()
 	{
 		return data.length;
